28e381819344af70900f7b75994b1aa934cc5a19
ajit51 <ajitk433@gmail.com>
2024-02-08 15:06:16 +0530

    added RequestMapping

--- src/main/java/com/example/gitexample/controller/MyController.java
+++ src/main/java/com/example/gitexample/controller/MyController.java
@@ -4,9 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class MyController {
 
     private Logger logger = LoggerFactory.getLogger(MyController.class);
